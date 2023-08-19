4156751468c609506c9550d6f0de1ff7dcaad928
Tasnimul Hasan Samit <57057963+samithasan142@users.noreply.github.com>
2023-08-19 17:26:35 +0600

    Update Binary Search.cpp

--- Binary Search/Binary Search.cpp
+++ Binary Search/Binary Search.cpp
@@ -1,32 +1,44 @@
 /* Binary Search */
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+using namespace std;
 
-int bin_search(int A[], int n, int target)
-{
-    int left, right, mid;
-    left = 0;
-    right = n-1;
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size()-1;
 
-    while(left <= right){
-        mid = (left + right) / 2;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
 
-        if(A[mid] == target){
-            return mid;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] < target){
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
         }
-        else if(A[mid] < target){
-            left = mid + 1;
-        }
-        else right = mid - 1;
+        return -1;
     }
-    return -1;
-}
+};
 
-int main()
-{
-    int A[] = {1, 4, 6, 7, 10, 19, 22, 23, 30, 35, 39, 46};
-    int target = 22;
-    int result = bin_search(A, 12, target);
-    printf("%d", result);
+int main() {
+    Solution sol;
+    vector<int> nums = {-1, 0, 3, 5, 9, 12};
+    int target = 9;
+
+    int result = sol.search(nums, target);
+
+    if (result != -1) {
+        cout << "Target " << target << " found at index " << result << endl;
+    }
+    else {
+        cout << "Target " << target << " not found in the array." << endl;
+    }
+
     return 0;
 }
