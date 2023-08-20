6ee780b21ddac538149c76f858332b65ecef3b58
Tasnimul Hasan Samit <57057963+samithasan142@users.noreply.github.com>
2023-08-21 01:02:52 +0600

    Add files via upload

---
+++ Binary Tree/Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        queue<TreeNode*> q;
+        if(root == NULL) return ans;
+
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();
+            vector<int>level;
+            for(int i=0; i<size; i++){
+                TreeNode*node = q.front();
+                q.pop();
+
+                if(node->left != NULL){
+                    q.push(node->left);
+                }
+                if(node->right != NULL){
+                    q.push(node->right);
+                }
+                level.push_back(node->val);
+            }
+            ans.push_back(level);
+        }
+        return ans;
+    }
+};
+
+int main() {
+    // Creating a binary tree
+    TreeNode* root = new TreeNode(3);
+    root->left = new TreeNode(9);
+    root->right = new TreeNode(20);
+    root->right->left = new TreeNode(15);
+    root->right->right = new TreeNode(7);
+
+    Solution sol;
+    vector<vector<int>> result = sol.levelOrder(root);
+
+    cout << "Level Order Traversal:" << endl;
+    for (int i = 0; i < result.size(); i++) {
+        for (int j = 0; j < result[i].size(); j++) {
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+/*
+        _3_
+       /   \
+      9     20
+           /  \
+          15   7
+
+    Level order Traversal: 3 9 20 15 7
+*/
+
+
