874a1e42b762d475095f4634bae33bb4c7b1ea91
Tasnimul Hasan Samit <57057963+samithasan142@users.noreply.github.com>
2023-08-22 22:00:48 +0600

    Add files via upload

---
+++ Binary Search Tree/Kth Smallest Element in a BST.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    vector<int> ans;
+    void inorderTraversal(TreeNode* root){
+        if(root == NULL) return;
+
+        inorderTraversal(root->left);
+        ans.push_back(root->val);
+        inorderTraversal(root->right);
+    }
+
+    int kthSmallest(TreeNode* root, int k) {
+        inorderTraversal(root);
+        return ans[k-1];
+    }
+};
+
+int main() {
+    TreeNode* root = new TreeNode(3);
+    root->left = new TreeNode(1);
+    root->right = new TreeNode(4);
+    root->left->right = new TreeNode(2);
+
+    Solution sol;
+
+    int k = 3;
+    int value = sol.kthSmallest(root, k);
+    cout << "The kth smallest element is: " << value << endl;
+
+    return 0;
+}
