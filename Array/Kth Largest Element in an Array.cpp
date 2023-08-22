342885cc79ba6c540b4311dcaceae770cd6e2f8f
Tasnimul Hasan Samit <57057963+samithasan142@users.noreply.github.com>
2023-08-22 22:19:36 +0600

    Add files via upload

---
+++ Array/Kth Largest Element in an Array.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end(), greater<int>());
+        return nums[k-1];
+    }
+};
+
+int main() {
+    vector<int> nums = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
+    int k = 3;
+
+    Solution sol;
+    int largest = sol.findKthLargest(nums, k);
+    cout << "The kth largest element is: " << largest << std::endl;
+
+    return 0;
+}
+
+/*
+    Complexity:
+    Time complexity: O(n*log(n)) for sorting
+    Space complexity: O(1)
+*/
+
+/*
+//Solution 2: Max-Heap Approach (Time complexity: O(n*log(k)))
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq(nums.begin(), nums.end());
+        for (int i = 0; i < k - 1; i++)
+            pq.pop();
+        return pq.top();
+    }
+};
+*/
