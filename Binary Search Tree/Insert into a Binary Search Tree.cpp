116a8e49102ae251a75313c2b14a117ef23fd311
Tasnimul Hasan Samit <57057963+samithasan142@users.noreply.github.com>
2023-08-23 12:58:48 +0600

    Add files via upload

---
+++ Binary Search Tree/Insert into a Binary Search Tree.cpp
@@ -0,0 +1,55 @@
+/* Leetcode Problem no: 701 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root == NULL){
+            return new TreeNode(val);
+        }
+        else if(val < root->val){
+            root->left = insertIntoBST(root->left, val);
+        }else{
+            root->right = insertIntoBST(root->right, val);
+        }
+        return root;
+    }
+};
+
+// Helper function to print inorder traversal of the BST
+void inorderTraversal(TreeNode* root) {
+    if (root == nullptr) return;
+
+    inorderTraversal(root->left);
+    cout << root->val << " ";
+    inorderTraversal(root->right);
+}
+
+int main(){
+    Solution sol;
+
+    TreeNode* root = new TreeNode(4);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(7);
+    root->left->left = new TreeNode(1);
+    root->left->right = new TreeNode(3);
+
+    int newValue = 5; // The value we want to insert
+    root = sol.insertIntoBST(root, newValue);
+
+    cout << "Inorder traversal of the modified BST: ";
+    inorderTraversal(root);
+    cout << endl;
+
+    return 0;
+}
