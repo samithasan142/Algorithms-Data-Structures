8f5b50f51b9c0e281ed34d40ff91ba17a7c933a8
Tasnimul Hasan Samit <57057963+samithasan142@users.noreply.github.com>
2023-08-26 01:16:11 +0600

    Add files via upload

---
+++ Fibonacci Number without Recursion.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fib(int n){
+    int num1=0, num2=1, nextTerm;
+    if(n<=1){
+        return n;
+    }
+    for(int i=2; i<=n; i++){
+        nextTerm = num1 + num2;
+        num1 = num2;
+        num2 = nextTerm;
+    }
+    return nextTerm;
+}
+int main(){
+    int n = 10;
+    cout << fib(n);
+    return 0;
+}
